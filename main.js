10ab854392897b3b5bb95cf67bf226bc0f198197
Colin Jaffe <balloonasaurus@gmail.com>
2020-04-15 11:47:28 -0400

    Finalizes solution.

--- main.js
+++ main.js
@@ -2,7 +2,67 @@
  * YOUR CODE HERE *
  ******************/
 
+const Grade = (assignment, score) => {
+  return {
+    assignment,
+    score,
+  }
+}
 
+const Term = (hours) => {
+  return {
+    hours,
+    grades: [],
+
+    addGrade: function(assignment, score) {
+      const grade = Grade(assignment, score);
+      this.grades.push(grade);
+    },
+  }
+}
+
+const Course = (name) => {
+  return {
+    name,
+    terms: [],
+
+    addTerm: function(hours) {
+      const term = Term(hours);
+      this.terms.push(term);
+    }
+  }
+}
+
+const Student = (name, course, term = 1) => {
+  return {
+    name,
+    course,
+    term,
+    courses: [],
+
+    addCourse: function(name) {
+      this.courses.push(Course(name));
+    },
+
+    getAverage: function(courseToFind = this.course, termIndex = this.term - 1) {
+      let foundCourse;
+      for (const course of this.courses) {
+        if (course.name === courseToFind) {
+          foundCourse = course;
+        }
+      }
+
+      console.log(foundCourse);
+      let sum = 0;
+      const grades = foundCourse.terms[termIndex].grades;
+      for (const grade of grades) {
+        sum += grade.score;
+      }
+
+      return sum / grades.length;
+    },
+  }
+}
 
 
 
@@ -11,11 +71,26 @@
  * OUR CODE BELOW; DO NOT TOUCH! *
  *********************************/
 
-if (typeof exampleFunction === 'undefined') {
-  exampleFunction = undefined;
+if (typeof Grade === 'undefined') {
+  Grade = undefined;
+}
+
+if (typeof Term === 'undefined') {
+  Term = undefined;
+}
+
+if (typeof Course === 'undefined') {
+  Course = undefined;
+}
+
+if (typeof Student === 'undefined') {
+  Student = undefined;
 }
 
 
 module.exports = {
-  exampleFunction,
+  Grade,
+  Term,
+  Course,
+  Student,
 }
